85946e2ca1e20b5e1414e20c4d5a79e11fac1fab
Carson Jardine <carsonjardine@gmail.com>
2020-12-14 10:04:26 -0700

    Update controller and view to pass tests

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,5 +1,13 @@
 class FoodsController < ApplicationController
   def index
+    ingredient = params[:q]
 
+    conn = Faraday.new(url: "https://api.nal.usda.gov") do |f|
+      f.params["api_key"] = ENV["FDC_API_KEY"]
+    end
+
+    response = conn.get("fdc/v1/foods/search?&query=#{ingredient}&pageSize=10")
+    @json = JSON.parse(response.body, symbolize_names: true)
+    @foods = @json[:foods]
   end
 end

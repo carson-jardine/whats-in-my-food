6618ce34018c148f68815d8dd5d8b45bbf80102a
Carson Jardine <carsonjardine@gmail.com>
2020-12-14 09:26:56 -0700

    Update tests for foods index

--- spec/features/user_can_search_by_ingredient_spec.rb
+++ spec/features/user_can_search_by_ingredient_spec.rb
@@ -2,22 +2,38 @@ require 'rails_helper'
 
 describe 'As a User' do
   describe 'When I fill in the search form with "sweet potatoes"' do
-    it "I am taken to the foods index page" do
-      # As a user,
-      # When I visit "/"
-      # And I fill in the search form with "sweet potatoes"
-      # (Note: Use the existing search form)
-      # And I click "Search"
-      # Then I should be on page "/foods"
+    it "I am taken to the foods index page and see a total number of search results" do
       visit '/'
 
       expect(page).to have_field(:q)
       fill_in :q, with: 'sweet potatoes'
       click_button('Search')
       expect(current_path).to eq('/foods')
+      expect(page).to have_content('10 Results')
+    end
+
+    it "I see a list of ten foods that contain sweet potatoes and their details" do
+      visit '/'
+      fill_in :q, with: 'sweet potatoes'
+      click_button('Search')
+
+      expect(page).to have_css(".food", count: 10)
+      within(first(".food")) do
+        expect(page).to have_css(".code")
+        expect(page).to have_css(".description")
+        expect(page).to have_css(".brand_owner")
+        expect(page).to have_css(".ingredients")
+      end
     end
   end
 end
+# As a user,
+# When I visit "/"
+# And I fill in the search form with "sweet potatoes"
+# (Note: Use the existing search form)
+# And I click "Search"
+# Then I should be on page "/foods"
+
 # Then I should see a total of the number of items returned by the search.
 # Then I should see a list of ten foods that contain the ingredient "sweet potatoes"
 # And for each of the foods I should see:
